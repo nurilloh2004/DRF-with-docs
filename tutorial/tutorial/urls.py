8a80711a8c51f5f525ba24ec3cf7949d3540e6ab
nurilloh2004 <nurillohpython@gmail.com>
2022-12-05 09:43:30 +0500

    all

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,21 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 from django.urls import include, path
 from rest_framework import routers
 from tutorial.quickstart import views
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
+
+
+
+
+
